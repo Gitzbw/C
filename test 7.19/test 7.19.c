b17fbb3fa5c9322e2e1cfce5ae5d82b19382cc1c
Gitzbw <81919800@qq.com>
2021-08-01 22:45:22 +0800

    test

---
+++ test 7.19/test 7.19.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include "test 7.19.h"
+
+//int 整型
+//main前面的int表示main函数调用返回一个整型值
+//int main(void)//主函数 程序的入口 有且仅有一个
+//{
+//	//完成任务
+//	printf("hello world"); //打印函数
+//	
+//
+//
+//
+//
+//	return 0;// 返回 0
+//
+//}
+  
+
+//int main(void)
+//{
+//
+//
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//
+//
+//	printf("sum = %d",sum);
+//
+//
+//	return 0;
+//
+//}
+
+
+//int main(void)
+//{
+//
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c','\0'};
+//	printf("%s\n",arr1);
+//	printf("%s\n",arr2);
+//
+//	
+//
+//	return 0;
+//
+//
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
