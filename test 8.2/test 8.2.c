c22d22f76d45119a64129fb12738224c48d15205
Gitzbw <81919800@qq.com>
2021-08-02 22:52:28 +0800

    test

---
+++ test 8.2/test 8.2.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//struct S
+//{
+//	int n;
+//	int* arr;
+//};
+//
+//int main(void)
+//{
+//	struct S* p = (struct S*)malloc(sizeof(struct S));
+//	p->n = 100;
+//	p->arr = (int*)malloc(40);
+//	
+//		int i = 0;
+//		for (i = 0; i < 10; i++)
+//		{
+//			p->arr[i] = i;
+//		}
+//		for (i = 0; i < 10; i++)
+//		{
+//			printf("%d ",p->arr[i]);
+//		}
+//		free(p->arr);
+//		p->arr = NULL;
+//		free(p);
+//		p = NULL;
+//	
+//	
+//
+//	return 0;
+//}
+
+
+
+//int main(void)
+//{
+//
+//	int ch = fgetc(stdin);
+//	fputc(ch, stdout);
+//
+//	return 0;
+//}
