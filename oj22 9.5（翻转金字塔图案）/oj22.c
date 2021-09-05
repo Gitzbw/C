792029ed98841a2258bebbd7955eb3b4ed789eb7
Gitzbw <81919800@qq.com>
2021-09-05 10:28:07 +0800

    oj22

---
+++ oj22 9.5（翻转金字塔图案）/oj22.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 0;
+	while (~scanf(" %d", &n))
+	{
+		int i = 0;
+		for (i=0;i<n;i++)
+		{
+			int j = 0;
+			for (j=0;j<i;j++)
+			{
+				printf(" ");
+			}
+			for (j = 0; j < n-i; j++)
+			{
+				printf("* ");
+			}
+			printf("\n");
+		}
+
+
+	}
+
+
+	return 0;
+}
