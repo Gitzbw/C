0055149092e62da13a4d8ca95b1093cd6ab14150
Gitzbw <81919800@qq.com>
2021-09-07 09:20:52 +0800

    oj24

---
+++ oj24 9.7（K形图案）/oj24.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+
+	int n = 0;
+	while (~scanf(" %d", &n))
+	{
+		int i = 0;
+		for (i = 0; i < n+1; i++)
+		{
+			int j = 0;
+			for (j = 0; j < n+1-i; j++)
+			{
+				printf("* ");
+
+			}
+			printf("\n");
+		}
+		for (i = 0; i < n; i++)
+		{
+			int j = 0;
+			for (j = 0; j < i+2; j++)
+			{
+				printf("* ");
+
+			}
+			
+			printf("\n");
+		}
+
+
+	}
+
+
+
+
+	return 0;
+}
